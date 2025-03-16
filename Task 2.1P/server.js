6120583fdeb738d66b258b9414526ca0368cb69b
Saly222 <139831340+Saly222@users.noreply.github.com>
2025-03-16 23:03:30 +1100

    Add files via upload

---
+++ Task 2.1P/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(express.static(__dirname));
+
+
+
+app.listen(PORT, () =>
+{
+    console.log(`Server is running on port ${PORT}`)
+})
